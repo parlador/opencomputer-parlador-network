18dd07143a7343ebbe61e244d47513296d9ce43d
parlador <jean.olivier.bergeron@gmail.com>
2019-05-07 20:38:41 -0400

    Update reactor_control.lua

--- app/reactor_control.lua
+++ app/reactor_control.lua
@@ -262,9 +262,13 @@ OutputRate = application:addChild(GUI.text(24, 20, 0x999999, "0 /T"))
 
 --------------------------------------------------------------------------------
 -- control zone
-application:addChild(GUI.panel(1, 23, 27, 10, 0x2D2D2D))
+application:addChild(GUI.panel(1, 23, 27, 5, 0x2D2D2D))
 
-application:addChild(GUI.panel(29, 23, 56, 10, 0x2D2D2D))
+application:addChild(GUI.panel(29, 23, 55, 5, 0x2D2D2D))
+
+application:addChild(GUI.panel(1, 29, 27, 5, 0x2D2D2D))
+
+application:addChild(GUI.panel(29, 29, 55, 5, 0x2D2D2D))
  
  
  
