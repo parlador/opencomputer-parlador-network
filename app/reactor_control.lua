0f38aff42f6235a1c27f8abb727c759ecf9ce0bc
parlador <jean.olivier.bergeron@gmail.com>
2019-05-11 16:54:30 -0400

    Update reactor_control.lua

--- app/reactor_control.lua
+++ app/reactor_control.lua
@@ -293,15 +293,18 @@ end
 
 
  -- line1
-application:addChild(GUI.panel(57, 2, 50, 10, 0x2D2D2D))
+application:addChild(GUI.panel(57, 2, 50, 11, 0x2D2D2D))
 application:addChild(GUI.panel(57, 2, 50, 1, 0x1F4582))
-chartTemperature = application:addChild(GUI.chart(57, 3, 50, 10, 0xEEEEEE, 0xAAAAAA, 0x888888, 0x5F63FE, 1, 1, "s", "C", true, {}))
 --application:addChild(GUI.text(45, 10, 0xFFFFFF, "   FUEL TEMPERATURE       "))
+chartTemperature = application:addChild(GUI.chart(57, 3, 50, 10, 0xEEEEEE, 0xAAAAAA, 0x888888, 0x5F63FE, 1, 1, "s", "C", true, {}))
+application:addChild(GUI.text(69, 2, 0x000000, "                                                  "))
 table.insert(chartTemperature.values,1,{0, 0})
 
-application:addChild(GUI.panel(108, 2, 50, 10, 0x2D2D2D))
-chartFuel = application:addChild(GUI.chart(109, 3, 50, 10, 0xEEEEEE, 0xAAAAAA, 0x888888, 0xD2DE67, 1, 1, "s", "%", true, {}))
+application:addChild(GUI.panel(109, 2, 50, 11, 0x2D2D2D))
+application:addChild(GUI.panel(109, 2, 50, 1, 0x1F4582))
 --application:addChild(GUI.text(73, 10, 0xFFFFFF, "       FUEL TANK           "))
+chartFuel = application:addChild(GUI.chart(109, 3, 50, 10, 0xEEEEEE, 0xAAAAAA, 0x888888, 0xD2DE67, 1, 1, "s", "%", true, {}))
+application:addChild(GUI.text(120, 2, 0x000000, "                                                  "))
 table.insert(chartFuel.values,1,{0, 0})
  
 --line 2
