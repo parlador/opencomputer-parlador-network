7c5a9fc32ef2d301e3e0a096db4b49fc794456e8
parlador <jean.olivier.bergeron@gmail.com>
2019-05-11 18:58:57 -0400

    Update dieselgenerator_control.lua

--- app/dieselgenerator_control.lua
+++ app/dieselgenerator_control.lua
@@ -1,1 +1,11 @@
 -- diesel generator control
+
+conduit saturation:  (enderio_conduit_bundle)
+pwr_getMaxPowerInconduits
+pwr_getPowerInconduits
+
+
+enderio normal bank (activated_capbank)
+getCurrentStorage
+getMaximumStorage
+setOuputControlMode (IGNORE/""ON")
