b745b8c7ffec46dc093caac99b70b0f02e3a9758
parlador <jean.olivier.bergeron@gmail.com>
2020-03-06 17:14:02 -0500

    Update 01-screen.lua

--- startup/init.d/01-screen.lua
+++ startup/init.d/01-screen.lua
@@ -7,6 +7,8 @@ local screen = component.screen
   if screenblockW == 2 and screenblockH == 1 then
     os.execute("resolution 160 35")
   end 
-
+  if screenblockW == 2 and screenblockH == 2 then
+    os.execute("resolution 100 50")
+  end 
 
   
