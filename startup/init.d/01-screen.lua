2513b3e0b0940f9eaa9777f7044e7be7811596f5
parlador <jean.olivier.bergeron@gmail.com>
2020-03-05 22:13:12 -0500

    Update 01-screen.lua

--- startup/init.d/01-screen.lua
+++ startup/init.d/01-screen.lua
@@ -1,1 +1,8 @@
+local component = require("component")
+local screen = component.screen
 
+if screen.isPrecise() then
+  print("Not supported screen")
+endif
+  
+-- os.execute("rm /tmp/setup/base/"..i)
