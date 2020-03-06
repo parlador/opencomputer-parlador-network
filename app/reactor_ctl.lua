8e9fa22c46c1fba272e151d99327dabd34d3e3f1
parlador <jean.olivier.bergeron@gmail.com>
2020-03-06 17:35:05 -0500

    Update reactor_ctl.lua

--- app/reactor_ctl.lua
+++ app/reactor_ctl.lua
@@ -1,9 +1,16 @@
 local GUI = require("GUI")
 component = require("component")
-
+Reactorlist = {}
  
 --------------------------------------------------------------------------------
  
 for address, name in component.list("bigreactor", false) do
   print(name.." : "..address)
+  Reactorlist[address]["addr"]=address
+end
+
+
+
+for i,line in ipairs(Reactorlist) do
+    print(line["addr"])
 end
