741d288040c213e3f18971a9a1707600d74f5729
parlador <jean.olivier.bergeron@gmail.com>
2020-03-05 23:05:05 -0500

    Update 01-screen.lua

--- startup/init.d/01-screen.lua
+++ startup/init.d/01-screen.lua
@@ -2,7 +2,13 @@ local component = require("component")
 local screen = component.screen
 
 if screen.isPrecise() then
-  print("Not supported screen")
+  local screenblockW, screenblockH = screen.getAspectRatio()
+  if screenblockW == 2 and screenblockH == 1 then
+    os.execute("resolution 160 35
+  end 
 end
   
 -- os.execute("rm /tmp/setup/base/"..i)
+
+
+-- 1H 2W 160 35
