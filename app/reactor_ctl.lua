ec5f11bee1d7cac74ee74f346e0713be1c267e99
parlador <jean.olivier.bergeron@gmail.com>
2020-03-07 15:30:33 -0500

    Update reactor_ctl.lua

--- app/reactor_ctl.lua
+++ app/reactor_ctl.lua
@@ -3,12 +3,42 @@ component = require("component")
 Reactorlist = {}
  
 --------------------------------------------------------------------------------
+
+function GetPourcentagePower(ReactorAdresse)
+    CurrentPower = component.invoke(ReactorAdresse, "getEnergyStored")
+    MaxPower = 10000000
+    return round((100 /MaxPower) * CurrentPower, 1)
+end
+function GetPourcentageFuel(ReactorAdresse)
+    CurrentFuel = component.invoke(ReactorAdresse, "getFuelAmount")
+    MaxFuel = component.invoke(ReactorAdresse, "getFuelAmountMax")
+    return round((100 /MaxFuel) * CurrentFuel, 1)
+end
+function GetPourcentageWaste(ReactorAdresse)
+    CurrentWaste = component.invoke(ReactorAdresse, "getWasteAmount")
+    MaxFuel = component.invoke(ReactorAdresse, "getFuelAmountMax")
+    return round((100 /MaxFuel) * CurrentWaste, 1)
+end
+function GetPourcentageHotFuel(ReactorAdresse)
+    CurrentHotFuel = component.invoke(ReactorAdresse, "getHotFluidAmount")
+    MaxHotFluid = component.invoke(ReactorAdresse, "getHotFluidAmountMax")
+    return round((100 /MaxHotFluid) * CurrentHotFuel, 1)
+end
  
-for address, name in component.list("bigreactor", false) do
-  print(name.." : "..address)
-  Reactorlist[address]={"addr"=address}
+--------------------------------------------------------------------------------
+
+function PollReactors()
+      tmpReactorList  = {}
+      idreactor = 1
+      for address, name in component.list("bigreactor", false) do
+        print(name.." : "..address)
+        tmpReactorList[idreactor]={"Address"=address,"PourcentageHotFuel"=GetPourcentageHotFuel(address),"PourcentageWaste"=GetPourcentageWaste(address)."PourcentageFuel"=GetPourcentageFuel(address),"PourcentagePower"=GetPourcentagePower(address)}
+        idreactor += 1
+      end
+  return tmpReactorList
 end
 
+Reactorlist = PollReactors()
 
 
 for i,line in ipairs(Reactorlist) do
