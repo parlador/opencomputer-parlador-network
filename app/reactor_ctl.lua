b64847d276dda37e2e9ac93c77b9667c8e57556c
parlador <jean.olivier.bergeron@gmail.com>
2020-03-07 16:24:56 -0500

    Update reactor_ctl.lua

--- app/reactor_ctl.lua
+++ app/reactor_ctl.lua
@@ -57,19 +57,39 @@ function GetReactorActiver(ReactorAdresse)
 end 
 
 function GetSteamTrigger(ReactorAdresse)
-    Reactorlist[]
+    if Reactorlist[ReactorAdresse] then
+       return Reactorlist[ReactorAdresse]["SteamTrigger"]
+    else
+       return 95
+    end
 end
 function GetPowerTrigger(ReactorAdresse)
- 
+    if Reactorlist[ReactorAdresse] then
+       return Reactorlist[ReactorAdresse]["PowerTrigger"]
+    else
+       return 95
+    end
 end
 function GetTempLimit(ReactorAdresse)
- 
+    if Reactorlist[ReactorAdresse] then
+       return Reactorlist[ReactorAdresse]["TempLimit"]
+    else
+       return 975
+    end
 end
 function GetRodLevel(ReactorAdresse)
- 
+    if Reactorlist[ReactorAdresse] then
+       return Reactorlist[ReactorAdresse]["RodLevel"]
+    else
+       return 100
+    end
 end
 function GetRodLevelLimit(ReactorAdresse)
- 
+    if Reactorlist[ReactorAdresse] then
+       return Reactorlist[ReactorAdresse]["RodLevelLimit"]
+    else
+       return 100
+    end
 end
 
 --------------------------------------------------------------------------------
@@ -78,7 +98,7 @@ function PollReactors()
       tmpReactorList  = {}
       idreactor = 1
       for address, name in component.list("bigreactor", false) do
-        tmpReactorList[address]={Address=address,PourcentageHotFuel=GetPourcentageHotFuel(address),PourcentageWaste=GetPourcentageWaste(address),PourcentageFuel=GetPourcentageFuel(address),PourcentagePower=GetPourcentagePower(address),ActivelyCooled=GetActivelyCooled(address),CasingTemperature=GetCasingTemperature(address),FuelTemperature=GetFuelTemperature(address),FuelReactivity=GetFuelReactivity(address),FuelConsumedLastTick=GetFuelConsumedLastTick(address),ReactorMasterSwitch=true,RodLevelLimit=100,RodLevel=100,TempLimit=970,PowerTrigger=95,SteamTrigger=95,ReactorEnable=GetReactorActiver(address)}
+        tmpReactorList[address]={Address=address,PourcentageHotFuel=GetPourcentageHotFuel(address),PourcentageWaste=GetPourcentageWaste(address),PourcentageFuel=GetPourcentageFuel(address),PourcentagePower=GetPourcentagePower(address),ActivelyCooled=GetActivelyCooled(address),CasingTemperature=GetCasingTemperature(address),FuelTemperature=GetFuelTemperature(address),FuelReactivity=GetFuelReactivity(address),FuelConsumedLastTick=GetFuelConsumedLastTick(address),ReactorMasterSwitch=true,RodLevelLimit=GetRodLevelLimit(),RodLevel=GetRodLevel(address),TempLimit=GetTempLimit(address),PowerTrigger=GetPowerTrigger(address),SteamTrigger=GetSteamTrigger(address),ReactorEnable=GetReactorActiver(address)}
         idreactor = 1
       end
   return tmpReactorList
