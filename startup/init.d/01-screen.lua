d8b7958e2511c02629bbb2233b95f104d107aa79
parlador <jean.olivier.bergeron@gmail.com>
2020-03-07 22:20:54 -0500

    Update 01-screen.lua

--- startup/init.d/01-screen.lua
+++ startup/init.d/01-screen.lua
@@ -1,15 +1,14 @@
 local component = require("component")
+local gpu = component.gpu
 local screen = component.screen
-
-for k,v in component.list() do print(k, v) end
 
   local screenblockW, screenblockH = screen.getAspectRatio()
 
   if screenblockW == 2 and screenblockH == 1 then
-    os.execute("resolution 160 35")
+    gpu.resolution(160,35)
   end 
   if screenblockW == 2 and screenblockH == 2 then
-    os.execute("resolution 100 50")
+    gpu.resolution(100,50)
   end 
 
   
