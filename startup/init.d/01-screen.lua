bdfeb90c16958fd06437dc23fd768cde512d94b3
parlador <jean.olivier.bergeron@gmail.com>
2020-03-07 22:34:28 -0500

    Update 01-screen.lua

--- startup/init.d/01-screen.lua
+++ startup/init.d/01-screen.lua
@@ -12,10 +12,10 @@ local MaxW, MaxH = gpu.maxResolution()
   local screenblockW, screenblockH = screen.getAspectRatio()
 
   if screenblockW == 2 and screenblockH == 1 then
-    gpu.setresolution(160,35)
+    gpu.setResolution(160,35)
   end 
   if screenblockW == 2 and screenblockH == 2 then
-    gpu.setresolution(100,50)
+    gpu.setResolution(100,50)
   end 
 
   
