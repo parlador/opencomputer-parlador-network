201c624c24b6fcf8e82f9e0c72f1b0526397fbc0
parlador <jean.olivier.bergeron@gmail.com>
2020-03-07 22:22:49 -0500

    Update 01-screen.lua

--- startup/init.d/01-screen.lua
+++ startup/init.d/01-screen.lua
@@ -5,10 +5,10 @@ local screen = component.screen
   local screenblockW, screenblockH = screen.getAspectRatio()
 
   if screenblockW == 2 and screenblockH == 1 then
-    gpu.resolution(160,35)
+    gpu.setresolution(160,35)
   end 
   if screenblockW == 2 and screenblockH == 2 then
-    gpu.resolution(100,50)
+    gpu.setresolution(100,50)
   end 
 
   
